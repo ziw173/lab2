97f1d0e35be1ae5800b6f55d89d3df4808359194
Ziqi Wu <ziw173@ucsd.edu>
2018-01-18 11:46:02 -0800

    added Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -14,5 +14,15 @@ function initializePage() {
 	});
 
 	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	// example: $("#div-iod").click(functionToCall);
+}
+
+$("a.thumbnail").click(projectClick);
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
 }
